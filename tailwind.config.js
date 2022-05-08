9c25ce25519221b40f58df8b5678bf1193709063
AgusCFx <agusfx00@gmail.com>
2022-05-07 21:37:24 -0300

    index.html on root for view on Github-pages, add images on Design folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
     colors: {
       'black': '#000000',
       'whiteInactive': '#988F96',
-      'whiteactive': '#D8D6DC',
+      'whiteActive': '#D8D6DC',
       'greenInactive': '#378A0C',
       'greenActive': '#48DD00',
       'purpleInactive': '#290831',
